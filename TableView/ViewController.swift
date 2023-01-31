314f16dc0d402db28a144736c072cc8a7478f5c4
doshkor <dosh.kor@gmail.com>
2023-01-31 17:21:36 +0900

    feat: tableView 화면 출력 기능 추가

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokerHands.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell else { return UITableViewCell() }
+        cell.titleLabel.text = pokerHands[indexPath.row]
+        cell.detailLabel.text = possibilityRates[indexPath.row]
+        
+        return cell
     }
 }
 
 
 class TableViewCell: UITableViewCell {
     
-    @IBOutlet weak var datailLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
 }
