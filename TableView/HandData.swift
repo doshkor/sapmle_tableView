2baa59835577e678e7471a873025b632a8730252
doshkor <dosh.kor@gmail.com>
2023-01-31 17:00:44 +0900

    HandData.swift 데이터 파일 추가

---
+++ TableView/HandData.swift
@@ -0,0 +1,12 @@
+//
+//  HandData.swift
+//  TableView
+//
+//  Created by 신동오 on 2023/01/31.
+//
+
+import Foundation
+
+let pokerHands = ["High card", "One pair", "Two pairs", "Three of a kind", "Straight", "Flush", "Full house", "Four of a kind", "Straight flush", "Royal flush"]
+
+let possibilityRates = ["45.11%", "40.25%", "4.75%", "2.10%", "0.39%", "0.197%", "0.1441%", "0.024%", "0.00139%", "0.000154%"]
