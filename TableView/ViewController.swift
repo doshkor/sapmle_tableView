e5ed334106fd37581e5ebc106c2c076142f2a6eb
doshkor <dosh.kor@gmail.com>
2023-01-31 10:00:52 +0900

    first commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by 신동오 on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
